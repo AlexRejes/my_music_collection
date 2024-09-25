741d2e79ba9e90a4760ef67b8bcf08701c3601a9
Alexandre <alexandre.rejes@outlook.com>
2024-09-25 18:05:23 -0300

    Fix login system to authenticate using username and password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,18 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # Permite parâmetros personalizados para login e cadastro
+  def configure_permitted_parameters
+    # Permitir o uso do username no login
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :username ])
+
+    # Permitir os campos full_name, username e role durante o registro
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :full_name, :username, :role ])
+
+    # Permitir os campos full_name, username e role na edição do perfil
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :full_name, :username, :role ])
+  end
+
   # Redireciona o usuário para a página de login após o registro
   def after_sign_up_path_for(resource)
     new_user_session_path # Vai para a página de login após o cadastro
@@ -14,12 +26,8 @@ class ApplicationController < ActionController::Base
     albums_path
   end
 
+  # Redireciona para a página de login após o logout
   def after_sign_out_path_for(resource_or_scope)
-    new_user_session_path # Redireciona para a página de login após o logout
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :full_name, :username, :role ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :full_name, :username, :role ])
+    new_user_session_path
   end
 end
