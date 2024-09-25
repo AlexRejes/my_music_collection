974abb0665b2d9d6d856a308a73b15b2958bd782
Alexandre <alexandre.rejes@outlook.com>
2024-09-25 16:39:53 -0300

    Refactor: Implement logout using form without JavaScript and display user full name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  # Redireciona o usuário para a página de login após o registro
+  def after_sign_up_path_for(resource)
+    new_user_session_path # Vai para a página de login após o cadastro
+  end
+
+  # Redireciona o usuário para a página de álbuns após o login
+  def after_sign_in_path_for(resource)
+    albums_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path # Redireciona para a página de login após o logout
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :full_name, :username, :role ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :full_name, :username, :role ])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # Inclui os módulos do Devise
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  # Validações
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :full_name, presence: true
+
+  # Substitui a busca por email por username
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (username = conditions.delete(:username))
+      where(conditions).where([ "lower(username) = :value", { value: username.downcase } ]).first
+    else
+      where(conditions).first
+    end
+  end
 end
